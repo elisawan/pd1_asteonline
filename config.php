8db8a91e2da09f3611c55a01efa104bd6c94edbe
ElisaWan <elisa.zhimin.wan@gmail.com>
2017-06-20 15:22:59 +0200

    ADD config.php values for polito server

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
-  define('DBUSER', 'root');
-  define('DBPASS', '');
-  define('DBNAME', 'asteonline');
+
+//FOLDER_NAME: 0e13a1
+
+  define('DBUSER', 'root'); //s241854
+  define('DBPASS', ''); //enualeri
+  define('DBNAME', 'asteonline'); //s241854
   define('DBHOST', 'localhost');
 
   require_once('./include/OBJ_user.php');
   require_once('./include/OBJ_BID.php');
 
-  
+
 ?>
